6da8f2f1dc5900b26c0aa8290c83aeb5b69743fb
Gomathi6860 <gomathi@psgitech.ac.in>
2025-02-12 13:42:47 +0530

    Update MissingSeq.py

--- Assignment1/MissingSeq.py
+++ Assignment1/MissingSeq.py
@@ -1,1 +1,10 @@
-
+n=int(input("Enter the value of N:"))
+print("Enter ",n-1," space-separated numbers:")
+m=list(map(int,input().split()))
+numbers = list(range(1, n+1))
+l=[]
+for i in numbers:
+    if i not in m:
+        l.append(i)
+for i in l:
+    print(i,end=" ")
